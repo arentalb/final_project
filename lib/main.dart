02eab4baf72fe46b0ace28d4d3720ac9e2f75097
aren talb <aren.talb00@gmail.com>
2025-02-19 12:40:52 +0300

    update comment

--- lib/main.dart
+++ lib/main.dart
@@ -16,5 +16,9 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
+
+
+ //updatey amam krdwa 1
+
   runApp(MyApp(settingsController: settingsController));
 }
