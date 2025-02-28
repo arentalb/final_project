340a4f483e816d0dc045d7065a1d65c606bf2016
aren talb <aren.talb00@gmail.com>
2025-02-28 23:17:55 +0300

    Remove the factory on word model

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -17,27 +17,5 @@ class Word {
     required this.reviewCount,
   });
 
-  factory Word.fromJson(Map<String, dynamic> json) {
-    return Word(
-      id: json['id'] as String,
-      englishWord: json['englishWord'] as String,
-      kurdishWord: json['kurdishWord'] as String,
-      boxNumber: json['boxNumber'] as int,
-      lastReviewed: DateTime.parse(json['lastReviewed'] as String),
-      nextReviewDate: DateTime.parse(json['nextReviewDate'] as String),
-      reviewCount: json['reviewCount'] as int,
-    );
-  }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'englishWord': englishWord,
-      'kurdishWord': kurdishWord,
-      'boxNumber': boxNumber,
-      'lastReviewed': lastReviewed.toIso8601String(),
-      'nextReviewDate': nextReviewDate.toIso8601String(),
-      'reviewCount': reviewCount,
-    };
-  }
 }
